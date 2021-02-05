c08049ae4b5be9ac90d7e700e6b9bfedd959336b
Dencher12 <denischerry19@gmail.com>
2021-02-05 15:23:25 +0300

    A lot of shit

---
+++ DungeonLessons/Data/Migrations/20210131191229_CreateLessons.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DungeonLessons.Data.Migrations
+{
+    public partial class CreateLessons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lessons",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Title = table.Column<string>(maxLength: 256, nullable: false)
+                },
+                constraints: table => { table.PrimaryKey("PK_Lessons", x => x.Id); });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ DungeonLessons/Data/Migrations/20210201110027_AddTextToLessons.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DungeonLessons.Data.Migrations
+{
+    public partial class AddTextToLessons : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>("Text", "Lessons", "text", defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+          
+        }
+    }
+}
